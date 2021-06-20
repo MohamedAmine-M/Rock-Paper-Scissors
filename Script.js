dafed3440803216b109c1ae6d5937cd66d8354d9
Mohamed Amine <mohamedamineouerfelli32@gmail.com>
2021-06-21 00:02:08 +0100

    RPS script

--- Script.js
+++ Script.js
@@ -1,77 +1,121 @@
-function RPS(UserChoice, ResUserChoice){
-    var UserChoice = prompt("Pick [0]Rock , [1]Paper, [2]Scissors: ");
-    var ResUserChoice = "";
-    if (isNaN(parseInt(UserChoice))){
-        if (UserChoice.toUpperCase()===("ROCK")){
-            ResUserChoice = UserChoice.toLowerCase();
-            UserChoice = "0";
+function game(){
+    console.log("                ");
+
+    function userPlay(){
+        var UserChoice = prompt("Pick [0]Rock , [1]Paper, [2]Scissors: ");
+        var playerSelection = "";
+        if (isNaN(parseInt(UserChoice))){
+            if (UserChoice.toUpperCase()===("ROCK")){
+                playerSelection = UserChoice.toLowerCase();
+                UserChoice = "0";
+            }
+            else if (UserChoice.toUpperCase()===("PAPER")){
+                playerSelection = UserChoice.toLowerCase();
+                UserChoice = "1";
+            }
+            else if (UserChoice.toUpperCase()===("SCISSORS")){
+                playerSelection = UserChoice.toLowerCase();
+                UserChoice = "2";
+            }
         }
-        else if (UserChoice.toUpperCase()===("PAPER")){
-            ResUserChoice = UserChoice.toLowerCase();
-            UserChoice = "1";
+        else{
+            while((0> parseInt(UserChoice)) || (2< parseInt(UserChoice))){
+                UserChoice = prompt("Pick [0]Rock , [1]Paper, [2]Scissors: ");
+            }
+            if (UserChoice == 0 ){
+                playerSelection = "Rock";
+            }
+            else if (UserChoice == 1 ){
+                playerSelection = "Paper";
+            }
+            else if (UserChoice == 2 ){
+                playerSelection = "Scissors";
+            }
         }
-        else if (UserChoice.toUpperCase()===("SCISSORS")){
-            ResUserChoice = UserChoice.toLowerCase();
-            UserChoice = "2";
+        return UserChoice+playerSelection
+    }
+    
+    function computerPlay(){
+        var computerSelection = "";
+        if (ComputerChoice == 0 ){
+            computerSelection = "Rock";
         }
+        else if (ComputerChoice == 1 ){
+            computerSelection = "Paper";
+        }
+        else if (ComputerChoice == 2 ){
+            computerSelection = "Scissors";
+        }
+        return computerSelection
+    }
+    
+    let UserPicks = userPlay();
+    UserChoice = parseInt(UserPicks[0]);
+    playerSelection = UserPicks.slice(1);
+    
+    
+    var ComputerChoice = Math.floor(Math.random() * 3);
+    computerSelection = computerPlay();
+    
+    console.log("User Choice is: "+ playerSelection);
+    console.log("Computer Choice is: "+ computerSelection.toLowerCase());
+    
+    var Uscore = 0;
+    var pScore =0;
+    if ( ComputerChoice === UserChoice){
+        console.log("Draw");
+    }
+    else if ((ComputerChoice == 0) && (UserChoice == 1)){
+        console.log("User Won this round");
+        Uscore = Uscore +1;
+    }
+    else if ((ComputerChoice == 1) && (UserChoice == 2)){
+        console.log("User Won this round");
+        Uscore = Uscore +1;
+    }
+    else if ((ComputerChoice == 2) && (UserChoice == 0)){
+        console.log("User Won this round");
+        Uscore = Uscore +1;
     }
     else{
-        while((0> parseInt(UserChoice)) || (2< parseInt(UserChoice))){
-            UserChoice = prompt("Pick [0]Rock , [1]Paper, [2]Scissors: ");
-        }
-        if (UserChoice == 0 ){
-            ResUserChoice = "Rock";
-        }
-        else if (UserChoice == 1 ){
-            ResUserChoice = "Paper";
-        }
-        else if (UserChoice == 2 ){
-            ResUserChoice = "Scissors";
-        }
+        console.log("Computer Won this round");
+        pScore = pScore +1 ;
     }
-    return UserChoice+ResUserChoice
+    return [pScore, Uscore]
 }
-
-function CompChoice(){
-    var ResComputerChoice = "";
-    if (ComputerChoice == 0 ){
-        ResComputerChoice = "Rock";
-    }
-    else if (ComputerChoice == 1 ){
-        ResComputerChoice = "Paper";
-    }
-    else if (ComputerChoice == 2 ){
-        ResComputerChoice = "Scissors";
-    }
-    return ResComputerChoice
+alert("First Round");
+var UserPts = 0;
+var Ppts = 0 ;
+var score = game();
+UserPts = score[1] + UserPts;
+Ppts = score[0] + Ppts;
+alert("Second Round");
+score = game();
+UserPts = score[1] + UserPts;
+Ppts = score[0] + Ppts;
+alert("Third Round");
+score = game();
+UserPts = score[1] + UserPts;
+Ppts = score[0] + Ppts;
+alert("Fourth Round");
+score = game();
+UserPts = score[1] + UserPts;
+Ppts = score[0] + Ppts;
+alert("Last Round");
+score = game();
+UserPts = score[1] + UserPts;
+Ppts = score[0] + Ppts;
+console.log("     ");
+console.log("Final Results")
+if(Ppts > UserPts){
+    console.log("Computer is the winner");
 }
-
-let UserPicks = RPS();
-UserChoice = parseInt(UserPicks[0]);
-ResUserChoice = UserPicks.slice(1);
-
-
-var ComputerChoice = Math.floor(Math.random() * 3);
-ResComputerChoice = CompChoice();
-
-console.log("User Choice is: "+ ResUserChoice);
-console.log("Computer Choice is: "+ ResComputerChoice.toLowerCase());
-
-
-if ( ComputerChoice === UserChoice){
+else if ( Ppts == UserPts){
     console.log("Draw");
 }
-else if ((ComputerChoice == 0) && (UserChoice == 1)){
-    console.log("User Won");
-}
-else if ((ComputerChoice == 1) && (UserChoice == 2)){
-    console.log("User Won");
-}
-else if ((ComputerChoice == 2) && (UserChoice == 0)){
-    console.log("User Won");
-}
 else{
-    console.log("Computer Won");
+    console.log("User is the winner ");
 }
-
-
+console.log(" ")
+console.log("Computer score: ", + Ppts);
+console.log("User score: "+ UserPts);
